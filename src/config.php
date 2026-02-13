909bb604e3cef98fbf5a152318c1cb891ab02459
Ismael Althenofer <ialthenofer@tsgroup.com>
2026-02-13 11:56:43 -0300

    Remove sensitive files from repository and respect .gitignore

--- src/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-return [
-    'host' => getenv('DB_HOST') ?: 'db',
-    'user' => getenv('DB_USER') ?: 'admin',
-    'pass' => getenv('DB_PASS') ?: 'tienda',
-    'db'   => getenv('DB_NAME') ?: 'sistema_stock',
-];
