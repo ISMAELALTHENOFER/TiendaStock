8962b3eaebaa1212ceb3995c418e8baa5a5a545b
Ismael Althenofer <ialthenofer@tsgroup.com>
2026-02-27 23:30:32 -0300

    Agregar migraciones para las tablas 'categorias' y 'productos' con sus respectivas columnas y relaciones

---
+++ src/database/migrations/2026_02_28_022825_create_productos_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('descripcion')->nullable();
+            $table->foreignId('categoria_id')->constrained('categorias')->onDelete('cascade');
+            $table->decimal('precio_compra', 10, 2);
+            $table->decimal('precio_venta', 10, 2);
+            $table->integer('cantidad');
+            $table->string('talle')->nullable();
+            $table->string('color')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('productos');
+    }
+};
